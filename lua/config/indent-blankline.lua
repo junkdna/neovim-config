455372ea9b8d7a7ae975889bec6996feb49796d8
Tillmann Heidsieck <theidsieck@leenox.de>
2024-06-03 15:34:25 +0200

    make indent blankeline more tame
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -4,4 +4,22 @@ if not is_ok then
 end
 
 -- For all available options, take a look at `:help ibl.config.`
-indent_blankline.setup()
+indent_blankline.setup {
+    indent = {
+        -- highlight = highlight,
+        char = "│",
+        priority = 100,
+    },
+    whitespace = {
+        --highlight = { "Function", "Label" },
+        highlight = nil,
+        remove_blankline_trail = false,
+    },
+    scope = {
+        enabled = false,
+        show_start = true,
+        show_end = false,
+        highlight = { "Function", "Label" },
+        priority = 500,
+    },
+}
