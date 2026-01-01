76e997d6550362c67d97eb12bd2ca277cd9dcd52
Tillmann Heidsieck <theidsieck@leenox.de>
2026-01-01 11:15:28 +0100

    plugins: general: add neogit
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -7,5 +7,27 @@ return {
             vim.o.background = 'dark'
             vim.cmd.colorscheme 'solarized'
         end,
-    },
+    },{
+        "NeogitOrg/neogit",
+        tag = "v3.0.0",
+        lazy = true,
+        dependencies = {
+            {
+                "nvim-lua/plenary.nvim",
+                commit = "b9fd5226c2f76c951fc8ed5923d85e4de065e509"
+            },
+            {
+                "sindrets/diffview.nvim",
+                commit = "4516612fe98ff56ae0415a259ff6361a89419b0a"
+            },
+            {
+                "nvim-telescope/telescope.nvim",
+                tag = "v0.2.1"
+            },
+        },
+        cmd = "Neogit",
+        keys = {
+            { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+        }
+    }
 }
