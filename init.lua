c7eab280faa7c58a31df925bb928ccc0fc61582d
Tillmann Heidsieck <theidsieck@leenox.de>
2026-01-05 18:16:13 +0100

    init: enable treesitter
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- init.lua
+++ init.lua
@@ -2,6 +2,11 @@ require("config.lazy")
 require("config.options")
 require("config.lsp")
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function() vim.treesitter.start() end,
+})
+
 vim.cmd[[
     colorscheme solarized-osaka
 ]]
