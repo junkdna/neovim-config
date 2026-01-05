996da01688fe4a44e03e6f308b5967924a804ed9
Tillmann Heidsieck <theidsieck@leenox.de>
2026-01-05 18:16:36 +0100

    enable more treesitter languages
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -1,6 +1,17 @@
 require('nvim-treesitter').setup {
     -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+    ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "lua",
+        "python",
+        "query",
+        "sh",
+        "vhdl",
+        "vim",
+        "vimdoc",
+    },
     sync_install = false,
     auto_install = true,
     ignore_install = {},
